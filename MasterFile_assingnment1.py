91cba13681879aab3de936c757d1e9366a66eec1
Abinav-karthikeyan <abhinavkarthikeyan1999@gmail.com>
2023-10-02 15:13:11 +0100

    Update MasterFile_assingnment1.py
    Trying to add a Folium map based on custom markers mapped to lat long based on API call

--- MasterFile_assingnment1.py
+++ MasterFile_assingnment1.py
@@ -1,9 +1,12 @@
 import json
 import os
+import pprint
 import plotly.express as px
 from itertools import chain
+import folium
 import pandas as pd
-
+import requests
+import pprint
 os.chdir("C:\\Users\\abhin\\OneDrive\\Documents\\Course Content\\Python programming")
 
 def load_json_file_named(file_name):
@@ -49,7 +52,34 @@ fig = px.sunburst(data_sunburst,path= ['regions', 'years'], values = 'budget_cou
 fig.show()
 
 
+map_nhs_hosp = folium.Map(location=[28.644800, 77.216721]).show_in_browser()
 
 
 
 
+
+def lat_lng_builder(regions):
+    url = 'https://api.geoapify.com/v1/geocode/search'
+    params = dict(
+    text= regions,
+    apiKey='c4380cfb076b4cdab76c167d26d5467c' )
+    resp = requests.get(url=url, params=params)
+    datamap = resp.json()
+    return datamap['features'][0]["geometry"]["coordinates"][::-1]
+ 
+
+regions_coordinates = list(lat_lng_builder(region) for region in regions)
+ 
+lat_lng_builder(regions[5])
+print(regions_coordinates)
+
+fig_layout=folium.Figure(height=350,width=550)
+map_main=folium.Map(location= lat_lng_builder("University of Edinburgh Business School, 29 Buccleuch Place ,Edinburgh"),tiles='cartodbpositron',zoom_start=8)
+fig_layout.add_child(map_main)
+custom_icon = folium.DivIcon(html='<i class="fas fa-person-dress fa-2x"></i>', icon_size=(30, 30))
+
+
+for index, nhslocs in enumerate(regions_coordinates):
+    folium.Marker(location = nhslocs, popup = regions[index]).add_to(map_main)
+
+map_main.show_in_browser()
